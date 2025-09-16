a82d0df6d87f59c97f7aa3df9727c361d4abc0bb
Jonathan <jonathanallen105@gmail.com>
2025-09-16 13:10:04 -0400

    created the weathericon and weatherdata components.

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,12 +1,13 @@
 import './WeatherForecast.css'
+import WeatherData from './WeatherData'
+import WeatherIcon from './WeatherIcon'
 
 const WeatherForecast = ({day, img, imgAlt, conditions, time}) => {
     return (
         <div className="weather">
             <h2>{day}</h2>
-            <img src={img} alt={imgAlt} />
-            <p><span>conditions: </span>{conditions}</p>
-            <p><span>time: </span>{time}</p>
+            <WeatherIcon img={img} imgAlt={imgAlt} />
+            <WeatherData conditions={conditions} time={time} />
         </div>
 
     )
